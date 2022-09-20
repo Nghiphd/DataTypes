0677b319a54707c22e3234582672569dbf0b89f0
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-09-20 11:07:39 -0700

    floating-point

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,7 +9,7 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         int i;
-        double f = 2.5;
+        float f = 2.5f;
         boolean flag = true;
     }
 }
