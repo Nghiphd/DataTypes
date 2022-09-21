6b7fbb596c91902cec11032d7618e587f144632d
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-09-21 11:17:58 -0700

    Operators

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,7 +8,13 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        int i;
-        float f = 2.5f;
+        int i = 10;
+        int f = 0;
+        f += i;
+        int flag = f;
+        i += 1;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
     }
 }
