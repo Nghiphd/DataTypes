7779cbd93fe5b5285411a34aaae4c1f38d81cbd2
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-09-22 10:55:48 -0700

    inital

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,8 +8,6 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        int i;
-        float f = 2.5f;
-        boolean flag = true;
+        System.out.println("Hello World!");
     }
 }
